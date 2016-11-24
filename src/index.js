2958d0cd89daac22473993c73fbbbea24a6fae99
burczu <b.dybowski@gmail.com>
2016-11-24 16:53:29 +0100

    index route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import App from './components/Main';
 import About from './components/About';
 import LayoutWrapper from './components/LayoutWrapper';
@@ -9,8 +9,8 @@ import LayoutWrapper from './components/LayoutWrapper';
 // Render the main component into the dom
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route component={LayoutWrapper}>
-      <Route path="/" component={App}/>
+    <Route path="/" component={LayoutWrapper}>
+      <IndexRoute component={App}/>
       <Route path="/about" component={About} />
     </Route>
   </Router>
